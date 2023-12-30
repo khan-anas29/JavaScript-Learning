8e4baf89f0d9385aba128d635148fb6c10742de6
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2023-12-30 09:51:40 +0530

    Solution of String Challenge to Convert First letter and Greet the user in which first char is Uppercase and rest are lower case.

--- index.js
+++ index.js
@@ -1,32 +1,12 @@
-// // String Concatination
-// var greet="Hello"
-// var myName="Anas"
+// Input from User
+var name="aNas KhaN"
+// getting first word and making it upper case
+var firstWord= name.slice(0,1)
+firstWord=firstWord.toUpperCase()
 
-// console.log(greet,myName);
-// console.log(greet+myName);
+// getting restword and making it lower case
+var remainWord = name.slice(1,name.length)
+remainWord=remainWord.toLowerCase()
 
-
-// String Length
-
-// console.log(myName.length);
-
-// var maxText=180;
-
-// var tweet=prompt("Enter your tweet")
-// var remainText=maxText-(tweet.length)
-
-// console.log("You have "+remainText+ "out of 180 left");
-
-
-// Slicing and Extracting
-// variable.slice(start,end)
-
-
-// var tweet="ASHdjhasjdhjadsjansdjjahsahsfjkhjfdjdkfnjkashdjkahdsjknkncjhsdkjfhakjshdlasASHdjhasjdhjadsjansdjjahsahsfjkhjfdjdkfnjkashdjkahdsjknkncjhsdkjfhakjshdlas"
-// var maxlen=80
-// console.log(tweet.slice(0,maxlen)+" This is your tweet which has only 80 characters outof " + tweet.length);
-
-// var tweetUnderMax= tweet.slice(0,maxlen)
-// console.log(tweetUnderMax);
-
-
+// Output
+console.log("Hello,"+firstWord+remainWord);
