aef366e568a0d650fb4e5e6322d84da728586488
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2023-12-30 08:39:43 +0530

    Js Variable Question:Cannot change or redeclare the variable to swap the numbers

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-// Variables
+function sample(){
+    var a="3"
+    var b="8"
 
+    // Do not change the line above
+    // Write your code on line 7-9
+
+
+
+    // Do no change the line below
+    console.log("a is "+ a);
+    console.log("b is "+ b);
+}
+
+sample()
