8ed4940e430a487b2a990046c9744267da389fbf
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-01 15:34:03 +0530

    Created BMI Calculator using Functions

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
-// Life in Week
+// BMI Calculator
+/*
+Bmi = wieght in kg / height * height in metre */
 
-var age;
-
-function lifeInWeek(age){
-    // var ageInDays=365*age
-    // var ageInWeek=52*age
-    // var ageInMonths=12*age
-
-    // var ageLeftInDays = (365*90)-ageInDays
-    // var ageLeftInWeek = (52*90)-ageInWeek
-    // var ageLeftInMonths = (12*90)-ageInMonths
-
-    var yearsRemain = 90 - age;
-    var ageLeftInDays = yearsRemain * 365
-    var ageLeftInWeek = yearsRemain * 52
-    var ageLeftInMonths = yearsRemain * 12
-
-    console.log(`You have ${ageLeftInDays} Days, ${ageLeftInWeek} weeks, ${ageLeftInMonths} months left `);
+function bmiCalculation(weight,height) {
+    var bmiCalc= (weight/(height*height))
+    return bmiCalc
 }
 
-lifeInWeek(21);
-lifeInWeek(56);
 
+var bmi=bmiCalculation(56,1.8)
+console.log(bmi);
