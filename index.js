5847787fe86b7886fe7d070b118321616057d2fb
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-01 15:14:26 +0530

    Created Paramter function to calculate remaine life in weeks if we have age limit 90 years

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-// KarelChallenge for Functions
-/* Put beaker in 5*5 diagonal */
+// Life in Week
 
-function main(){
-    putBeeper();
-  move();
-    second();
-    second();
-    second();
-    second();
- }
- 
- function second(){
-    turnLeft();
-    move();
-    putBeeper();
-    turnRight();
-    move();
- }
+var age;
+
+function lifeInWeek(age){
+    var ageInDays=365*age
+    var ageInWeek=52*age
+    var ageInMonths=12*age
+
+    var ageLeftInDays = (365*90)-ageInDays
+    var ageLeftInWeek = (52*90)-ageInWeek
+    var ageLeftInMonths = (12*90)-ageInMonths
+
+    console.log(`You have ${ageLeftInDays} Days, ${ageLeftInWeek} weeks, ${ageLeftInMonths} months left `);
+}
+
+lifeInWeek(21);
+lifeInWeek(56);
+
