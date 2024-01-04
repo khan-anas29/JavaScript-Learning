20018f932bc920d31526ba14f8a4dabb116437e5
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-04 14:40:34 +0530

    Created Random number Generator And Created a Fun Love Calculator

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-// BMI Calculator
+// Random Number Generator
 /*
-Bmi = wieght in kg / height * height in metre */
+Creating Fun Love calculator random generating numbers */
 
-function bmiCalculation(weight,height) {
-    var bmiCalc= (weight/(height*height))
-    return bmiCalc
+// Take 2 inputs from user
+var name1;
+var name2;
+
+function loveCal(name1,name2) {
+    // We need in range of 1 to 100
+    var num=Math.random() * 100
+    num=Math.floor(num)
+    num=num+1
+    console.log(num+"%");
 }
 
-
-var bmi=bmiCalculation(56,1.8)
-console.log(bmi);
+loveCal("sameer","Shahrukh")
