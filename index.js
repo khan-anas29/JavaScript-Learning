ee6159ae1896a091630751e6878192bd4d007b7f
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-04 14:43:16 +0530

    Remove Unnecessary things

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ Creating Fun Love calculator random generating numbers */
 var name1;
 var name2;
 
-function loveCal(name1,name2) {
+function loveCal() {
     // We need in range of 1 to 100
     var num=Math.random() * 100
     num=Math.floor(num)
@@ -14,4 +14,4 @@ function loveCal(name1,name2) {
     console.log(num+"%");
 }
 
-loveCal("sameer","Shahrukh")
+loveCal()
