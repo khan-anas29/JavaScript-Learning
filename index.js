d94eeb96434ad360017105b552bed200fb33f8ac
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-09 20:53:27 +0530

    Write a program that prints the number from 1 to 100 But for multiples of three print (Fizz) instead of the number and for the multiple of five print (Buzz) and for numbers which are multiple of 3 and 5 both print (FizzBuzz)

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 // Arrays
 
-var guestList=["Self",
-"Bhabhi",
-"Bhai",
-"Ammi",
-"Dady",
-"Chacha"]
+/*
+Write a program that prints the number from 1 to 100 But for multiples of three print "Fizz" instead of the number and for the multiple of five print "Buzz" and for numbers which are multiple of 3 and 5 both print "FizzBuzz*/
 
-// take name of user as input from prompt
-
-var name ="Sameer"
-
-if(guestList.includes(name)){
-    console.log("Welcome");
-}
-else{
-    console.log("Next Time");
+for (let index = 1; index <=100; index++) {
+    // console.log(index);  
+    if(index%3===0 && index%5===0){
+        console.log("FizzBuzz");
+    }
+    else if(index%3===0){
+        console.log("Fizz");
+    }  
+    else if(index%5===0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(index);
+    }
 }
