16d85b7a241e67a9de2970f893e5361735945ca2
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-09 22:01:59 +0530

    Completed 99 bottles challenge using for loop

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 // 99 Bottles Challenge
 
-var bottles = 100
-
 function Bottles99() {
-    while (bottles > 0) {
+    for (bottles=100; bottles>0;bottles--) {
 
         console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of the beer.\nTake one down and pass it around,${bottles - 1} bottles of beer on the wall.\n`);
         bottles--;
