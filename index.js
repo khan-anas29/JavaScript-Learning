f10b5c8cf0b9ce3829dbb31d3db2f4268967ac65
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-09 21:32:12 +0530

    Write a program that prints the number from 1 to 100 But for multiples of three print (Fizz) instead of the number and for the multiple of five print (Buzz) and for numbers which are multiple of 3 and 5 both print (FizzBuzz) Without loop

--- index.js
+++ index.js
@@ -1,20 +1,59 @@
 // Arrays
 
 /*
-Write a program that prints the number from 1 to 100 But for multiples of three print "Fizz" instead of the number and for the multiple of five print "Buzz" and for numbers which are multiple of 3 and 5 both print "FizzBuzz*/
+Write a program that prints the number from 1 to 100 But for multiples of three print (Fizz) instead of the number and for the multiple of five print (Buzz) and for numbers which are multiple of 3 and 5 both print (FizzBuzz)*/
 
-for (let index = 1; index <=100; index++) {
-    // console.log(index);  
-    if(index%3===0 && index%5===0){
-        console.log("FizzBuzz");
+// for (let index = 1; index <=100; index++) {
+//     // console.log(index);  
+//     if(index%3===0 && index%5===0){
+//         console.log("FizzBuzz");
+//     }
+//     else if(index%3===0){
+//         console.log("Fizz");
+//     }  
+//     else if(index%5===0){
+//         console.log("Buzz");
+//     }
+//     else{
+//         console.log(index);
+//     }
+// }
+
+var output = []
+var count = 1
+function fizzBuzz() {
+    if (count % 3 === 0 && count % 5 === 0) {
+        output.push("Fizzbuzz")
     }
-    else if(index%3===0){
-        console.log("Fizz");
-    }  
-    else if(index%5===0){
-        console.log("Buzz");
+    else if (count % 3 === 0) {
+        output.push("Fizz");
     }
-    else{
-        console.log(index);
+    else if (count % 5 === 0) {
+        output.push("Buzz");
     }
+    else {
+        output.push(count);
+    }
+    count++
+    console.log(output);
 }
+
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
+fizzBuzz()
