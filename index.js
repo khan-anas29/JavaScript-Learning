5296b4a77b016ccbbe3636fa922c5858389ab801
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-09 21:58:24 +0530

    Completed 99 bottles challenge using while loop

--- index.js
+++ index.js
@@ -1,59 +1,13 @@
-// Arrays
+// 99 Bottles Challenge
 
-/*
-Write a program that prints the number from 1 to 100 But for multiples of three print (Fizz) instead of the number and for the multiple of five print (Buzz) and for numbers which are multiple of 3 and 5 both print (FizzBuzz)*/
+var bottles = 100
 
-// for (let index = 1; index <=100; index++) {
-//     // console.log(index);  
-//     if(index%3===0 && index%5===0){
-//         console.log("FizzBuzz");
-//     }
-//     else if(index%3===0){
-//         console.log("Fizz");
-//     }  
-//     else if(index%5===0){
-//         console.log("Buzz");
-//     }
-//     else{
-//         console.log(index);
-//     }
-// }
+function Bottles99() {
+    while (bottles > 0) {
 
-var output = []
-var count = 1
-function fizzBuzz() {
-    if (count % 3 === 0 && count % 5 === 0) {
-        output.push("Fizzbuzz")
+        console.log(`${bottles} bottles of beer on the wall, ${bottles} bottles of the beer.\nTake one down and pass it around,${bottles - 1} bottles of beer on the wall.\n`);
+        bottles--;
     }
-    else if (count % 3 === 0) {
-        output.push("Fizz");
-    }
-    else if (count % 5 === 0) {
-        output.push("Buzz");
-    }
-    else {
-        output.push(count);
-    }
-    count++
-    console.log(output);
 }
 
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
-fizzBuzz()
+Bottles99()
