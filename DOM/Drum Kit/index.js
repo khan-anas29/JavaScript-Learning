402f4fb0edb3956cc591bdfa3ad24d684fe083e0
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-13 22:50:17 +0530

    Added Images using css and added this keyword to target the button which is activating the event to put audio in it according to its name for future use

--- DOM/Drum Kit/index.js
+++ DOM/Drum Kit/index.js
@@ -2,9 +2,15 @@
 
 // we will need loop since querySelectorAll does not works, using class name since we may use button tags in future also so it will be not efficient
 
+// var audio=new Audio("sounds/tom-1.mp3")
+// // Adding Sounds
+// audio.play()
+
 for(var i=0; i<document.querySelectorAll(".drum").length;i++){
     document.querySelectorAll(".drum")[i].addEventListener("click",function () {
-        alert("Hello")    
+        this.style.color = "white"
+
     })
 }
+
 
