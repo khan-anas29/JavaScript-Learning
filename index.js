dd9777968d803e71bcd31fd4fd84ce134c946e65
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-13 22:35:23 +0530

    Learn and Added Higher Order Functions

--- index.js
+++ index.js
@@ -1,31 +1,20 @@
-// Fibonacci Challenge
+// Higher Order Function
 
-
-
-function CalcFib(n) {
-    var t1=0;
-    var t2=1;
-    var t3;
-    var output=[];
-    if (n===1){
-        output=[t1]
-    }
-    else if(n===2){
-        output=[t1,t2]
-    }
-    else{
-        output=[t1,t2]
-        for (i = 2; i < n; i++){
-            t3=t1+t2
-            t1=t2
-            t2=t3
-            output.push(t3)
-        }
-    }
-    return output
+function add(num1,num2){
+    return num1 + num2
 }
 
-var a=CalcFib(10)
-console.log(a);
-var b=CalcFib(10)
-console.log(b);
+function subtract(num1,num2){
+    return num1 - num2
+}
+
+function multiply(num1,num2){
+    return num1 * num2
+}
+
+function divide(num1,num2){
+    return num1 / num2
+}
+
+function calculator(num1,num2,operator){
+    return operator(num1,num2)}
