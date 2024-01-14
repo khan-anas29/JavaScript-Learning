55af81208c73b7fd6750f54060fcdd4daa966c0e
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-14 22:04:13 +0530

    Objects ,their Methods

--- index.js
+++ index.js
@@ -22,3 +22,18 @@ function Student(name,age,branch){
 // creating new object directly by constructor function
 
 var student3 = new Student("Salman","25","Driving")
+
+
+// Methods of object 
+
+function Teacher(name,age,branch){
+    this.name=name,
+    this.age=age,
+    this.branch=branch
+    this.makepapers=function(){
+        console.log("I made Papers of Exams");
+    }
+}
+
+var teacher1= new Teacher("Liza",23,"Ai&Ds")
+// teacher1 will have makepapers by default
