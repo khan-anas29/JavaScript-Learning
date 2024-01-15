1d83ac7b0be76384ba119ed872cee20d77b9dd4c
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-15 20:15:17 +0530

    Manipulate styles using jquery

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,9 +1,20 @@
 // document.querySelector("h1")
 // // this is same as
-
 // jQeuery("h1")
 // // which is also same as
+// $("h1"); No difference while selecting one and queryselectorAll in jQuery
 
-// $("h1")
 
-$("h1").css("color","red")
+/*Adding css using js is not good practice instead we can add class using this js which had all the css required  */
+
+// $("h1").css("color","green")
+
+// var headingColor=$("h1").css("color");
+// console.log(headingColor);
+
+
+// Adding Css using  jQuery
+
+$("h1").addClass("big-text")
+$("button").addClass("btn")
+
