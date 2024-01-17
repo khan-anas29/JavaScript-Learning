4afbba021854bfe75302a7dafa26091e43f1d6d1
Mohd Anas <anascollege2129@eng.rizvi.edu.in>
2024-01-17 21:35:47 +0530

    Completed Till Step 6

--- Simon Game/game.js
+++ Simon Game/game.js
@@ -6,54 +6,7 @@ var buttonColors=["red","blue","green","yellow"]
 
 var userClickedPattern=[]
 
-
-// Generate Random Number between 0 and 3
-// function nextSequence() {
-    
-//     var randomNumber = Math.floor(Math.random()*4)
-    
-//     // Choosing Random Colors
-//     var randomChosenColors=buttonColors[randomNumber]
-    
-//     // Adding Random color in Empty array
-//     gamePattern.push(randomChosenColors)
-
-
-//     // Adding Flash and Sounds Effect to Button which is beign Matched
-//     switch (randomChosenColors) {
-//         case "red":
-//             $("#red").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-//             var red=new Audio("sounds/red.mp3")
-//             red.play()
-//             break;
-//         case "blue":
-//             $("#blue").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-//             var blue=new Audio("sounds/blue.mp3")
-//             blue.play()
-//             break;
-//         case "green":
-//             $("#green").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-//             var green=new Audio("sounds/green.mp3")
-//             green.play()
-//             break;
-//         case "yellow":
-//             $("#yellow").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-//             var yellow=new Audio("sounds/yellow.mp3")
-//             yellow.play()
-//             break;
-    
-//         default:
-//             var wrong=new Audio("sounds/wrong.mp3")
-//             wrong.play()
-//             break;
-//     }
-// }
-
-
-// Getting the button which trigger handler after click
-
-
-
+// Getting which button is pressed
 $(".btn").click(function(){
     
     // this will give which color is clicked by providing its id
@@ -66,7 +19,7 @@ $(".btn").click(function(){
     
 });
 
-
+// generating Next Sequence
 function nextSequence() {
 
     var randomNumber = Math.floor(Math.random() * 4);
@@ -84,3 +37,13 @@ function playSound(name){
     var audio= new Audio("sounds/" + name + ".mp3")
     audio.play()
 }
+
+
+// Animation To Buttons
+
+function animatePress(currentColor) { 
+    $("."+currentColor).addClass("pressed");
+    setTimeout(() => {
+        $("."+currentColor).removeClass("pressed");
+    }, 100);
+ }
